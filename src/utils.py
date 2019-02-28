185e591033778db8abcac75a8a1c97786c687d70
Maxime Mouchet <mouchmaxime@gmail.com>
2019-02-28 20:50:45 +0100

    ut

--- src/utils.py
+++ src/utils.py
@@ -89,3 +89,13 @@ class HashCodeProblem:
 
         f.close()
         return image_count, photos
+
+    def solve_instance(self, input_file):
+        p_input = self.parse_input(input_file)
+        photos = p_input[1]
+        photos_v = [photo for photo in photos if photo.orientation=="H"]
+        slides = [Slide([photo]) for photo in photos_v]
+        slide_show = SlideShow(slides)
+        score = slide_show.compute_score()
+        slide_show.write_output("./out/"+input_file)
+        return score
